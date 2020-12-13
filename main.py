07bef4e3096a120cbfb1e0713bedf0742e3f7926
PianoReflex <64155517+PianoReflex@users.noreply.github.com>
2020-12-13 10:23:12 -0800

    Finally fixed camera movement :)
    Added coordinates to __init__ method to make camera movement almost seamless.

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ HEIGHT = window.winfo_screenheight()  # finds height of window
 WIDTH = window.winfo_screenwidth()  # finds width of window
 window.geometry("{1}x{0}".format(int(HEIGHT / 2), int(WIDTH / 2)))
 
-c = tkinter.Canvas(window, height=HEIGHT, width=WIDTH,
+c = tkinter.Canvas(window, height=HEIGHT * 2, width=WIDTH * 2,
                    highlightthickness=0)
-c.place(relx=0.5, rely=0.5, anchor="center")  # put the canvas widget inside our window
+c.place(x=WIDTH/2 + (WIDTH/4), y=HEIGHT/2, anchor="center")  # put the canvas widget inside our window
 window.wm_attributes("-transparentcolor", "white")
 for i in range(0, HEIGHT, 10):
     c.create_line(i, 0, i, WIDTH, fill="light grey")
@@ -31,17 +31,19 @@ class Player:
         self.movement_speed = movement_speed
         self.color = color
 
-    def spawn(self, coords, movement, canv_coord_x, canv_coord_y):
+    def spawn(self, coords, movement, canv_coord_x, canv_coord_y, move):
         p = Image.open("tank.png")
         p2 = ImageTk.PhotoImage(p)
         p2.image = p2
 
         self.movement = movement
-        self.canv_coord_x = canv_coord_x
-        self.canv_coord_y = canv_coord_y
+        self.canv_coord_x = WIDTH/2 + (WIDTH/4)
+        self.canv_coord_y = HEIGHT/2
         self.player_coord_x = coords[0]
         self.player_coord_y = coords[1]
+        self.move = move
 
+        c.place(x = WIDTH/2 + (WIDTH/4), y = HEIGHT/2, anchor = "center")
         player2 = c.create_image(coords[0], coords[1], image=p2)
 
     def movement2(self, event="<Key>"):
@@ -56,9 +58,9 @@ class Player:
             p = Image.open("tank.png")
             p2 = ImageTk.PhotoImage(p)
             p2.image = p2
-            self.canv_coord_x -= 0.02
-            self.coords[0] += 12
-            c.place(relx=self.canv_coord_x, rely=self.canv_coord_y, anchor="center")
+            self.canv_coord_x -= self.move
+            self.coords[0] += self.move
+            c.place(x=self.canv_coord_x, y=self.canv_coord_y, anchor="center")
             player2 = c.create_image(self.coords[0], self.coords[1], image=p2)
 
         elif key == "Left":
@@ -71,9 +73,9 @@ class Player:
             p = Image.open("tank.png")
             p2 = ImageTk.PhotoImage(p)
             p2.image = p2
-            self.canv_coord_x += 0.02
-            self.coords[0] -= 12
-            c.place(relx=self.canv_coord_x, rely=self.canv_coord_y, anchor="center")
+            self.canv_coord_x += self.move
+            self.coords[0] -= self.move
+            c.place(x=self.canv_coord_x, y=self.canv_coord_y, anchor="center")
             player2 = c.create_image(self.coords[0], self.coords[1], image=p2)
         elif key == "Up":
             c.delete("all")
@@ -85,9 +87,9 @@ class Player:
             p = Image.open("tank.png")
             p2 = ImageTk.PhotoImage(p)
             p2.image = p2
-            self.canv_coord_y += 0.02
-            self.coords[1] -= 8.2
-            c.place(relx=self.canv_coord_x, rely=self.canv_coord_y, anchor="center")
+            self.canv_coord_y += self.move
+            self.coords[1] -= self.move
+            c.place(x=self.canv_coord_x, y=self.canv_coord_y, anchor="center")
             player2 = c.create_image(self.coords[0], self.coords[1], image=p2)
         elif key == "Down":
             c.delete("all")
@@ -99,9 +101,9 @@ class Player:
             p = Image.open("tank.png")
             p2 = ImageTk.PhotoImage(p)
             p2.image = p2
-            self.canv_coord_y -= 0.02
-            self.coords[1] += 8.2
-            c.place(relx=self.canv_coord_x, rely=self.canv_coord_y, anchor="center")
+            self.canv_coord_y -= self.move
+            self.coords[1] += self.move
+            c.place(x=self.canv_coord_x, y=self.canv_coord_y, anchor="center")
             player2 = c.create_image(self.coords[0], self.coords[1], image = p2)
 
 
@@ -109,8 +111,8 @@ def mouse_movement(event):
     x, y = event.x, event.y
 
 
-player = Player(10, [WIDTH/2, HEIGHT/2, 50, 50], "blue")
-player.spawn([WIDTH/2, HEIGHT/2, 50, 80], 3, 0.5, 0.5)
+player = Player(10, [WIDTH/2, HEIGHT/2 + (HEIGHT/4)], "blue")
+player.spawn([WIDTH/2, HEIGHT/2 + (HEIGHT/4)], 3, WIDTH/2, HEIGHT/2, 3)
 
 
 def main_game_loop():
